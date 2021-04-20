9d2af31bc848d09b7c9719639ff898c87abda827
wluyima <wyclif@openmrs.org>
2021-04-20 13:54:42 -0500

    Set external id to uuid instead of an attrbute

--- src/test/java/com/mekomsolutions/eip/route/TestConstants.java
+++ src/test/java/com/mekomsolutions/eip/route/TestConstants.java
@@ -2,10 +2,6 @@ package com.mekomsolutions.eip.route;
 
 public class TestConstants {
 	
-	public static final String PROP_ODOO_PRODUCT_CONCEPT_ATTRIB_TYPE_UUID = "openmrs.odoo.productRef.conceptAttribType.uuid";
-	
-	public static final String PROP_ODOO_QTY_UNITS_CONCEPT_ATTRIB_TYPE_UUID = "openmrs.odoo.qtyUnitsExternalId.conceptAttribType.uuid";
-	
 	public static final String EX_PROP_ODOO_ID = "odoo-id";
 	
 	public static final String EX_PROP_ODOO_USER_ID = "odoo-user-id";
@@ -19,10 +15,6 @@ public class TestConstants {
 	public static final String URI_ODOO_CREATE_CUSTOMER = "direct:odoo-create-customer";
 	
 	public static final String ODOO_BASE_URL = "http://odoo.com";
-	
-	public static final String ODOO_PRODUCT_CONCEPT_ATTRIB_TYPE_UUID = "a4464534-f5e2-4aab-a54e-1f1a2ec6d445";
-	
-	public static final String ODOO_QTY_UNITS_CONCEPT_ATTRIB_TYPE_UUID = "b4464534-f5e2-4aab-a54e-1f1a2ec6d445";
 	
 	public static final String LISTENER_URI = "direct:odoo-event-listener";
 	
