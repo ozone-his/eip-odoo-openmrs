18af553576e609f6700790ca3a8a55a287a810b8
wluyima <wyclif@openmrs.org>
2021-05-28 11:18:02 -0500

    EIP-14 Updated person name and address handler route work against refactored openmrs-eip 2.x

--- src/test/java/com/mekomsolutions/eip/route/OdooPersonNameAndAddressHandlerRouteTest.java
+++ src/test/java/com/mekomsolutions/eip/route/OdooPersonNameAndAddressHandlerRouteTest.java
@@ -1,11 +1,13 @@
 package com.mekomsolutions.eip.route;
 
 import static com.mekomsolutions.eip.route.OdooTestConstants.EX_PROP_ENTITY;
-import static com.mekomsolutions.eip.route.OdooTestConstants.EX_PROP_TABLE_REPO_MAP;
-import static com.mekomsolutions.eip.route.OdooTestConstants.PATIENT_UUID;
+import static com.mekomsolutions.eip.route.OdooTestConstants.URI_FETCH_RESOURCE;
+import static com.mekomsolutions.eip.route.OdooTestConstants.URI_MOCK_FETCH_RESOURCE;
 import static com.mekomsolutions.eip.route.OdooTestConstants.URI_PERSON_NAME_ADDRESS_HANDLER;
 import static java.util.Collections.singletonMap;
-import static org.junit.Assert.assertNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
@@ -14,14 +16,6 @@ import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.support.DefaultExchange;
 import org.junit.Before;
 import org.junit.Test;
-import org.openmrs.eip.component.entity.Patient;
-import org.openmrs.eip.component.entity.PersonAddress;
-import org.openmrs.eip.component.entity.PersonName;
-import org.openmrs.eip.component.entity.light.PersonLight;
-import org.openmrs.eip.component.repository.PatientRepository;
-import org.openmrs.eip.component.repository.PersonAddressRepository;
-import org.openmrs.eip.component.repository.PersonNameRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import ch.qos.logback.classic.Level;
 
@@ -34,22 +28,18 @@ public class OdooPersonNameAndAddressHandlerRouteTest extends BaseOdooRouteTest 
 	@EndpointInject("mock:odoo-patient-handler")
 	private MockEndpoint mockPatientHandlerEndpoint;
 	
-	@Autowired
-	private PatientRepository patientRepo;
-	
-	@Autowired
-	private PersonNameRepository nameRepo;
-	
-	@Autowired
-	private PersonAddressRepository addressRepo;
+	@EndpointInject(URI_MOCK_FETCH_RESOURCE)
+	private MockEndpoint mockFetchResourceEndpoint;
 	
 	@Before
 	public void setup() throws Exception {
+		mockFetchResourceEndpoint.reset();
 		mockPatientHandlerEndpoint.reset();
 		advise(ROUTE_ID, new AdviceWithRouteBuilder() {
 			
 			@Override
 			public void configure() {
+				interceptSendToEndpoint(URI_FETCH_RESOURCE).skipSendToOriginalEndpoint().to(mockFetchResourceEndpoint);
 				interceptSendToEndpoint("direct:odoo-patient-handler").skipSendToOriginalEndpoint()
 				        .to(mockPatientHandlerEndpoint);
 			}
@@ -59,52 +49,67 @@ public class OdooPersonNameAndAddressHandlerRouteTest extends BaseOdooRouteTest 
 	
 	@Test
 	public void shouldLoadThePatientWhenProcessingPersonName() throws Exception {
-		Patient expectedPatient = patientRepo.findByUuid(PATIENT_UUID);
-		assertNotNull(expectedPatient);
+		final String personUuid = "person-uuid";
+		Map personResource = singletonMap("uuid", personUuid);
+		Map nameResource = new HashMap();
+		nameResource.put("uuid", "name-uuid");
+		nameResource.put("person", personResource);
+		Map patientResource = new HashMap();
+		patientResource.put("uuid", personUuid);
 		final Exchange exchange = new DefaultExchange(camelContext);
-		exchange.setProperty(EX_PROP_TABLE_REPO_MAP, singletonMap("patient", "patientRepository"));
-		PersonName personName = nameRepo.findByUuid("0bca417f-fc68-40d7-ae6f-cffca7a5eff1");
-		exchange.setProperty(EX_PROP_ENTITY, personName);
+		exchange.setProperty(EX_PROP_ENTITY, nameResource);
+		mockFetchResourceEndpoint.expectedMessageCount(1);
+		final String patientJson = mapper.writeValueAsString(patientResource);
+		mockFetchResourceEndpoint.whenAnyExchangeReceived(e -> e.getIn().setBody(patientJson));
 		mockPatientHandlerEndpoint.expectedMessageCount(1);
-		mockPatientHandlerEndpoint.expectedPropertyReceived(EX_PROP_PATIENT, expectedPatient);
+		mockPatientHandlerEndpoint.expectedPropertyReceived(EX_PROP_PATIENT, patientResource);
 		
 		producerTemplate.send(URI_PERSON_NAME_ADDRESS_HANDLER, exchange);
 		
+		mockFetchResourceEndpoint.assertIsSatisfied();
 		mockPatientHandlerEndpoint.assertIsSatisfied();
 	}
 	
 	@Test
 	public void shouldLoadThePatientWhenProcessingPersonAddress() throws Exception {
-		Patient expectedPatient = patientRepo.findByUuid(PATIENT_UUID);
-		assertNotNull(expectedPatient);
+		final String personUuid = "person-uuid";
+		Map personResource = singletonMap("uuid", personUuid);
+		Map addressResource = new HashMap();
+		addressResource.put("uuid", "address-uuid");
+		addressResource.put("person", personResource);
+		Map patientResource = new HashMap();
+		patientResource.put("uuid", personUuid);
 		final Exchange exchange = new DefaultExchange(camelContext);
-		exchange.setProperty(EX_PROP_TABLE_REPO_MAP, singletonMap("patient", "patientRepository"));
-		PersonAddress personAddress = addressRepo.findByUuid("359022bf-4a58-4732-8cce-1e57f72f47b0");
-		exchange.setProperty(EX_PROP_ENTITY, personAddress);
+		exchange.setProperty(EX_PROP_ENTITY, addressResource);
+		mockFetchResourceEndpoint.expectedMessageCount(1);
+		final String patientJson = mapper.writeValueAsString(patientResource);
+		mockFetchResourceEndpoint.whenAnyExchangeReceived(e -> e.getIn().setBody(patientJson));
 		mockPatientHandlerEndpoint.expectedMessageCount(1);
-		mockPatientHandlerEndpoint.expectedPropertyReceived(EX_PROP_PATIENT, expectedPatient);
+		mockPatientHandlerEndpoint.expectedPropertyReceived(EX_PROP_PATIENT, patientResource);
 		
 		producerTemplate.send(URI_PERSON_NAME_ADDRESS_HANDLER, exchange);
 		
+		mockFetchResourceEndpoint.assertIsSatisfied();
 		mockPatientHandlerEndpoint.assertIsSatisfied();
 	}
 	
 	@Test
 	public void shouldFailIfNoPatientIsFound() throws Exception {
-		final String badUuid = "bad-uuid";
+		final String badPersonUuid = "bad-uuid";
+		Map personResource = singletonMap("uuid", badPersonUuid);
+		Map addressResource = new HashMap();
+		addressResource.put("uuid", "address-uuid");
+		addressResource.put("person", personResource);
 		final Exchange exchange = new DefaultExchange(camelContext);
-		exchange.setProperty(EX_PROP_TABLE_REPO_MAP, singletonMap("patient", "patientRepository"));
-		PersonLight person = new PersonLight();
-		person.setUuid(badUuid);
-		PersonAddress personAddress = new PersonAddress();
-		personAddress.setPerson(person);
-		exchange.setProperty(EX_PROP_ENTITY, personAddress);
+		exchange.setProperty(EX_PROP_ENTITY, addressResource);
+		mockFetchResourceEndpoint.expectedMessageCount(1);
+		mockFetchResourceEndpoint.whenAnyExchangeReceived(e -> e.getIn().setBody(null));
 		mockPatientHandlerEndpoint.expectedMessageCount(0);
 		
 		producerTemplate.send(URI_PERSON_NAME_ADDRESS_HANDLER, exchange);
 		
 		mockPatientHandlerEndpoint.assertIsSatisfied();
-		assertMessageLogged(Level.WARN, "No associated patient found with uuid: " + badUuid);
+		assertMessageLogged(Level.WARN, "No associated patient found with uuid: " + badPersonUuid);
 	}
 	
 }
