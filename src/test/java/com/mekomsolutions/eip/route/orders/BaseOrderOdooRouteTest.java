6c96c5369d5be57a606e85f9999145798d4c300d
wluyima <wyclif@openmrs.org>
2022-02-02 12:22:55 +0300

    TFS-171831 Fixed failing tests

--- src/test/java/com/mekomsolutions/eip/route/orders/BaseOrderOdooRouteTest.java
+++ src/test/java/com/mekomsolutions/eip/route/orders/BaseOrderOdooRouteTest.java
@@ -4,7 +4,7 @@ import org.springframework.test.context.TestPropertySource;
 
 import com.mekomsolutions.eip.route.BaseOdooRouteTest;
 
-@TestPropertySource(properties = "camel.springboot.xml-routes=classpath*:camel/*.xml,classpath*:camel/orders/*.xml")
+@TestPropertySource(properties = "camel.springboot.xml-routes=classpath*:camel/orders/*.xml")
 @TestPropertySource(properties = "eip.watchedTables=patient,person_name,person_address,patient_identifier,orders,test_order,drug_order")
 @TestPropertySource(properties = "odoo.handler.route=odoo-order-handler")
 @TestPropertySource(properties = "odoo.custom.table.resource.mappings=orders:order,test_order:order,drug_order:order")
