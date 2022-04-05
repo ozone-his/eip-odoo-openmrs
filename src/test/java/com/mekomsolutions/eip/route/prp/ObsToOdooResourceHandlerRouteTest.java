654b94741fe89f0de827ce5a162c344f011edac1
wluyima <wyclif@openmrs.org>
2022-04-05 14:26:06 -0500

    TFS-302749 - Added obs-to-discharge-calendar-event and fixed obs-to-odoo-resource-handler routes

--- src/test/java/com/mekomsolutions/eip/route/prp/ObsToOdooResourceHandlerRouteTest.java
+++ src/test/java/com/mekomsolutions/eip/route/prp/ObsToOdooResourceHandlerRouteTest.java
@@ -1,12 +1,14 @@
 package com.mekomsolutions.eip.route.prp;
 
 import static com.mekomsolutions.eip.route.OdooTestConstants.APP_PROP_NAME_OBS_TO_ODOO_HANDLER;
+import static com.mekomsolutions.eip.route.OdooTestConstants.ROUTE_ID_OBS_TO_ADMISSION_EVENT;
 import static com.mekomsolutions.eip.route.OdooTestConstants.ROUTE_ID_OBS_TO_CUSTOMER;
+import static com.mekomsolutions.eip.route.OdooTestConstants.ROUTE_ID_OBS_TO_DISCHARGE_EVENT;
 import static com.mekomsolutions.eip.route.OdooTestConstants.ROUTE_ID_OBS_TO_RES_HANDLER;
-import static com.mekomsolutions.eip.route.OdooTestConstants.ROUTE_ID_SAVE_CALENDAR_EVENT;
+import static com.mekomsolutions.eip.route.OdooTestConstants.URI_OBS_TO_ADMISSION_EVENT;
 import static com.mekomsolutions.eip.route.OdooTestConstants.URI_OBS_TO_CUSTOMER;
+import static com.mekomsolutions.eip.route.OdooTestConstants.URI_OBS_TO_DISCHARGE_EVENT;
 import static com.mekomsolutions.eip.route.OdooTestConstants.URI_OBS_TO_RES_HANDLER;
-import static com.mekomsolutions.eip.route.OdooTestConstants.URI_SAVE_CALENDAR_EVENT;
 
 import org.apache.camel.EndpointInject;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
@@ -22,20 +24,25 @@ public class ObsToOdooResourceHandlerRouteTest extends BasePrpRouteTest {
 	@EndpointInject("mock:" + ROUTE_ID_OBS_TO_CUSTOMER)
 	private MockEndpoint mockObsToCustomerEndpoint;
 	
-	@EndpointInject("mock:" + ROUTE_ID_SAVE_CALENDAR_EVENT)
-	private MockEndpoint mockSaveCalendarEndpoint;
+	@EndpointInject("mock:" + ROUTE_ID_OBS_TO_ADMISSION_EVENT)
+	private MockEndpoint mockAdmissionEndpoint;
+	
+	@EndpointInject("mock:" + ROUTE_ID_OBS_TO_DISCHARGE_EVENT)
+	private MockEndpoint mockDischargeEndpoint;
 	
 	@Before
 	public void setup() throws Exception {
 		mockObsToCustomerEndpoint.reset();
-		mockSaveCalendarEndpoint.reset();
+		mockAdmissionEndpoint.reset();
+		mockDischargeEndpoint.reset();
 		
 		advise(ROUTE_ID_OBS_TO_RES_HANDLER, new AdviceWithRouteBuilder() {
 			
 			@Override
 			public void configure() {
 				interceptSendToEndpoint(URI_OBS_TO_CUSTOMER).skipSendToOriginalEndpoint().to(mockObsToCustomerEndpoint);
-				interceptSendToEndpoint(URI_SAVE_CALENDAR_EVENT).skipSendToOriginalEndpoint().to(mockSaveCalendarEndpoint);
+				interceptSendToEndpoint(URI_OBS_TO_ADMISSION_EVENT).skipSendToOriginalEndpoint().to(mockAdmissionEndpoint);
+				interceptSendToEndpoint(URI_OBS_TO_DISCHARGE_EVENT).skipSendToOriginalEndpoint().to(mockDischargeEndpoint);
 			}
 			
 		});
@@ -43,13 +50,15 @@ public class ObsToOdooResourceHandlerRouteTest extends BasePrpRouteTest {
 	
 	@Test
 	public void shouldCallTheAppropriateRoutes() throws Exception {
-		mockObsToCustomerEndpoint.assertIsSatisfied();
-		mockSaveCalendarEndpoint.assertIsSatisfied();
+		mockObsToCustomerEndpoint.expectedMessageCount(1);
+		mockAdmissionEndpoint.expectedMessageCount(1);
+		mockDischargeEndpoint.expectedMessageCount(1);
 		
 		producerTemplate.send(URI_OBS_TO_RES_HANDLER, new DefaultExchange(camelContext));
 		
 		mockObsToCustomerEndpoint.assertIsSatisfied();
-		mockSaveCalendarEndpoint.assertIsSatisfied();
+		mockAdmissionEndpoint.assertIsSatisfied();
+		mockDischargeEndpoint.assertIsSatisfied();
 	}
 	
 }
