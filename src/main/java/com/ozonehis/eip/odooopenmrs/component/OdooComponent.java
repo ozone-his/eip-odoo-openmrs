6d3298a38121242acd436ceb9b027882e7034f3e
Siddharth Vaish <sidvaish97@gmail.com>
2024-05-15 00:03:12 +0530

    OZ-545: Disable Consumer for Odoo component and code refactoring

--- src/main/java/com/ozonehis/eip/odooopenmrs/component/OdooComponent.java
+++ src/main/java/com/ozonehis/eip/odooopenmrs/component/OdooComponent.java
@@ -3,15 +3,21 @@ package com.ozonehis.eip.odooopenmrs.component;
 import org.apache.camel.Endpoint;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
+import org.openmrs.eip.mysql.watcher.MySqlWatcherComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 @Component("odoo")
 public class OdooComponent extends DefaultComponent {
 
+    private static final Logger logger = LoggerFactory.getLogger(MySqlWatcherComponent.class);
+
+
     @Override
-    protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
-        OdooEndpoint odooEndpoint = new OdooEndpoint();
-        return odooEndpoint;
+    protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) {
+        logger.info("Creating odoo endpoint with parameters: {}", parameters);
+        return new OdooEndpoint(uri, this);
     }
 }
--- src/main/java/com/ozonehis/eip/odooopenmrs/component/OdooEndpoint.java
+++ src/main/java/com/ozonehis/eip/odooopenmrs/component/OdooEndpoint.java
@@ -1,18 +1,23 @@
 package com.ozonehis.eip.odooopenmrs.component;
 
-import org.apache.camel.Category;
-import org.apache.camel.Consumer;
-import org.apache.camel.Processor;
-import org.apache.camel.Producer;
+import org.apache.camel.*;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.support.DefaultEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Odoo component to integrate with Odoo XML RPC.
  */
-@UriEndpoint(firstVersion = "1.0.0", scheme = "odoo", title = "Odoo", syntax = "odoo:apiName/methodName",
-        apiSyntax = "apiName/methodName", category = {Category.API})
+@UriEndpoint(firstVersion = "1.0.0", scheme = "odoo", title = "Odoo", syntax = "odoo:apiName/methodName", producerOnly = true)
 public class OdooEndpoint extends DefaultEndpoint {
+
+    private static final Logger log = LoggerFactory.getLogger(OdooEndpoint.class);
+
+    public OdooEndpoint(String endpointUri, Component component) {
+        this.setEndpointUri(endpointUri);
+        this.setComponent(component);
+    }
 
     @Override
     public String getEndpointBaseUri() {
@@ -20,7 +25,8 @@ public class OdooEndpoint extends DefaultEndpoint {
     }
 
     @Override
-    public Producer createProducer() throws Exception {
+    public Producer createProducer() {
+        log.info("Creating odoo producer");
         return new OdooProducer(this);
     }
 
@@ -31,6 +37,6 @@ public class OdooEndpoint extends DefaultEndpoint {
 
     @Override
     public Consumer createConsumer(Processor processor) {
-        throw new RuntimeException("Consumer not implemented");
+        throw new UnsupportedOperationException();
     }
 }
