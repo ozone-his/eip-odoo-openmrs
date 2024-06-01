a0c62ce8ca439ebdd09ce25a1d1ee518b7997de4
Siddharth Vaish <sidvaish97@gmail.com>
2024-06-01 23:34:57 +0530

    OZ-545: Fix sale order line description

--- src/main/java/com/ozonehis/eip/odooopenmrs/mapper/odoo/SaleOrderLineMapper.java
+++ src/main/java/com/ozonehis/eip/odooopenmrs/mapper/odoo/SaleOrderLineMapper.java
@@ -28,7 +28,7 @@ public class SaleOrderLineMapper<R extends Resource> implements ToOdooMapping<R,
             saleOrderLine.setSaleOrderLineProductUomQty(1.0f); // default quantity is 1 for serviceRequests.
             String requesterDisplay = serviceRequest.getRequester().getDisplay();
             String serviceDisplay = serviceRequest.getCode().getText();
-            saleOrderLine.setSaleOrderLineName(serviceDisplay + " | Requester: " + requesterDisplay);
+            saleOrderLine.setSaleOrderLineName(serviceDisplay + " | Orderer: " + requesterDisplay);
 
         } else if (resource instanceof MedicationRequest medicationRequest) {
             if (medicationRequest.hasDispenseRequest()) {
@@ -40,24 +40,12 @@ public class SaleOrderLineMapper<R extends Resource> implements ToOdooMapping<R,
                 }
             }
 
-            if (medicationRequest.hasMedicationReference()) {
-                if (medicationRequest.getMedicationReference().hasReference()) {
-                    String medicationCode = medicationRequest
-                            .getMedicationReference()
-                            .getReference()
-                            .split("/")[1];
-                    saleOrderLine.setSaleOrderLineName(medicationCode);
-                }
-            }
-
             String requesterDisplay = medicationRequest.getRequester().getDisplay();
             String medicationDisplay =
                     medicationRequest.getMedicationReference().getDisplay();
-            saleOrderLine.setSaleOrderLineName(saleOrderLine.getSaleOrderLineName() + " | " + medicationDisplay
-                    + " | Requester: " + requesterDisplay);
-            // Add dosage instructions to the notes.
-            saleOrderLine.setSaleOrderLineName(saleOrderLine.getSaleOrderLineName() + " | Notes: "
-                    + constructDosageInstructionsText(medicationRequest));
+            saleOrderLine.setSaleOrderLineName(
+                    medicationDisplay + " | " + constructDosageInstructionsText(medicationRequest, saleOrderLine)
+                            + " | Orderer: " + requesterDisplay);
 
         } else {
             throw new IllegalArgumentException("Sales Order Mapper Unsupported resource type: "
@@ -66,19 +54,18 @@ public class SaleOrderLineMapper<R extends Resource> implements ToOdooMapping<R,
         return saleOrderLine;
     }
 
-    /**
-     * Construct dosage instructions text from the medication request. Format: DOSE 10 tablet — oral — thrice daily — for 10
-     * days — REFILLS 1 — QUANTITY 30 Tablet
-     *
-     * @param medicationRequest medication request
-     * @return dosage instructions text
-     */
-    protected String constructDosageInstructionsText(MedicationRequest medicationRequest) {
+    protected String constructDosageInstructionsText(MedicationRequest medicationRequest, SaleOrderLine saleOrderLine) {
         Dosage dosage = medicationRequest.getDosageInstructionFirstRep();
         StringBuilder dosageInstructions = new StringBuilder();
 
         if (dosage.hasDoseAndRate()) {
-            dosageInstructions.append("DOSE ");
+            if (saleOrderLine.getSaleOrderLineProductUomQty() != null) {
+                dosageInstructions.append(saleOrderLine.getSaleOrderLineProductUomQty());
+                dosageInstructions.append(" ");
+                dosageInstructions.append(
+                        dosage.getDoseAndRateFirstRep().getDoseQuantity().getUnit());
+                dosageInstructions.append(" | ");
+            }
             dosageInstructions.append(
                     dosage.getDoseAndRateFirstRep().getDoseQuantity().getValue());
             dosageInstructions.append(" ");
@@ -87,28 +74,18 @@ public class SaleOrderLineMapper<R extends Resource> implements ToOdooMapping<R,
         }
 
         if (dosage.hasRoute()) {
-            dosageInstructions.append(" — ");
+            dosageInstructions.append(" - ");
             dosageInstructions.append(dosage.getRoute().getText());
         }
 
         if (dosage.hasTiming()) {
-            dosageInstructions.append(" — ");
+            dosageInstructions.append(" - ");
             dosageInstructions.append(dosage.getTiming().getCode().getText());
-            dosageInstructions.append(" — for ");
+            dosageInstructions.append(" - ");
             dosageInstructions.append(dosage.getTiming().getRepeat().getDuration());
             dosageInstructions.append(" ");
             dosageInstructions.append(
                     dosage.getTiming().getRepeat().getDurationUnit().getDisplay());
-        }
-        if (medicationRequest.hasDispenseRequest()) {
-            dosageInstructions.append(" — REFILLS ");
-            dosageInstructions.append(medicationRequest.getDispenseRequest().getNumberOfRepeatsAllowed());
-            dosageInstructions.append(" - QUANTITY ");
-            dosageInstructions.append(
-                    medicationRequest.getDispenseRequest().getQuantity().getValue());
-            dosageInstructions.append(" ");
-            dosageInstructions.append(
-                    medicationRequest.getDispenseRequest().getQuantity().getUnit());
         }
         return dosageInstructions.toString();
     }
