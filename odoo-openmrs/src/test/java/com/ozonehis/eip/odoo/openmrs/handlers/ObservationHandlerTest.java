63867e6b22415a915b2ab16f7c27fa4067ba661f
corneliouzbett <kipchubett@gmail.com>
2025-09-29 13:27:00 +0300

    Remove integration test + refactor test imports to use JUnit 5 assertions

--- odoo-openmrs/src/test/java/com/ozonehis/eip/odoo/openmrs/handlers/ObservationHandlerTest.java
+++ odoo-openmrs/src/test/java/com/ozonehis/eip/odoo/openmrs/handlers/ObservationHandlerTest.java
@@ -7,8 +7,8 @@
  */
 package com.ozonehis.eip.odoo.openmrs.handlers;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
